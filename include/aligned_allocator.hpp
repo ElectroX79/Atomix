1fcb3efbbc090a3368b6b9d09ae235de97221d67
ElectroX79 <jizhuoxian@gmail.com>
2026-08-01 01:42:42 +0200

    feat(aligned_allocator): add aligned allocator for memory allocation
    - Only for posix systems, using posix_memalign for aligned memory allocation.

---
+++ include/aligned_allocator.hpp
@@ -0,0 +1,46 @@
+#ifndef ALIGN_ALLOC_HPP
+#define ALIGN_ALLOC_HPP
+#include <cstdint>
+#include <stdexcept>
+#include <system_error>
+
+
+namespace atomix::mem::aligned_allocator{
+
+    [[nodiscard]]inline uint8_t* allocate(const size_t size, const size_t alignment = 64) {
+
+        const size_t real_size = size + alignment - (size % alignment);
+        const auto ptr = static_cast<uint8_t *>(aligned_alloc(alignment, real_size));
+
+        if (ptr == nullptr) {
+            if (errno == ENOMEM) {
+                const int error = errno;
+                throw std::system_error(error, std::system_category(), "Out of memory");
+            }
+            if (errno == EINVAL) {
+                throw std::invalid_argument("The alignment argument was not a power of two.");
+            }
+            throw std::bad_alloc();
+        }
+        return ptr;
+    }
+
+    inline void deallocate (uint8_t* ptr) {
+        if (ptr == nullptr) {
+            throw std::invalid_argument("The pointer cannot be nullptr");
+        }
+        free(reinterpret_cast<void*>(ptr));
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+#endif
