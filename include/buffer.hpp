13d365ac2c6170491b49221d178ebd26a01efeba
ElectroX79 <jizhuoxian@gmail.com>
2026-08-01 02:08:57 +0200

    feat(buffer, allocation_type): Add allocation type support to buffer, buffer now support the change with mem_route
    - In buffer.hpp are added the allocation type and the handle for automatic unmap, and the buffer now support the change with mem_route and the handle.

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -3,54 +3,54 @@
 
 #include <cstdint>
 #include <stdexcept>
-#include <vmem.hpp>
+#include "mem_route.hpp"
+
+#include "allocation_type.hpp"
 
 
-namespace atomix {
+namespace atomix::mem {
     class Buffer {
 
     private:
-        uint8_t* begin_ = nullptr;
-        size_t size_ = 0;
-
+        uint8_t* begin_;
+        size_t size_;
+        AllocationType type_;
+        VmemHandler* vmem_handler_;
 
     public:
 
-        Buffer(): begin_(nullptr), size_(0) {}
+        Buffer(uint8_t* begin, const size_t size, const AllocationType type, VmemHandler* vmem_handler): begin_(begin), size_(size), type_(type), vmem_handler_(vmem_handler){}
         Buffer(const Buffer&) = delete;
         Buffer& operator=(const Buffer&) = delete;
 
-        Buffer(Buffer&& other) noexcept: begin_(other.begin_), size_(other.size_) {
-            // no need to deallocate yet (using contructor)
+        Buffer(Buffer&& other)noexcept: begin_(other.begin_), size_(other.size_), type_(other.type_), vmem_handler_(other.vmem_handler_){
+            // no need to deallocate yet (using constructor)
             other.begin_ = nullptr;
             other.size_ = 0;
+            other.type_ = AllocationType::none;
+            other.vmem_handler_ = nullptr;
         }
 
         Buffer& operator=(Buffer&& other) noexcept {
             if (this != &other) {
-                vmem::deallocate(begin_, size_);
-                begin_ = nullptr;
-                size_ = 0;
+                mem_route::deallocate(begin_, size_, type_, vmem_handler_);
 
                 begin_ = other.begin_;
                 size_ = other.size_;
+                type_ = other.type_;
+                vmem_handler_ = other.vmem_handler_;
 
                 other.begin_ = nullptr;
                 other.size_ = 0;
+                other.type_ = AllocationType::none;
+                other.vmem_handler_ = nullptr;
             }
             return *this;
 
         }
 
-        explicit Buffer(const size_t size) {
-            begin_ = vmem::allocate(size);
-            size_ = size;
-        }
-
         ~Buffer(){
-            vmem::deallocate(begin_, size_);
-            begin_ = nullptr;
-            size_ = 0;
+            mem_route::deallocate(begin_, size_, type_, vmem_handler_);
         }
 
         [[nodiscard]] uint8_t* get_begin() const{
@@ -59,6 +59,10 @@ namespace atomix {
 
         [[nodiscard]] size_t size() const {
             return size_;
+        }
+
+        [[nodiscard]] AllocationType alloc_type()const {
+            return type_;
         }
 
     };
