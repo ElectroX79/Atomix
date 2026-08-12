60dfbb7874069a85147ab5f6a18823cb8c70e2cf
ElectroX79 <jizhuoxian@gmail.com>
2026-08-12 02:28:55 +0200

    feat: improve the bash script to be more scalable and measure the consumed resources

--- test/benchmark/gen_file.sh
+++ test/benchmark/gen_file.sh
@@ -1,19 +1,56 @@
+#!/usr/bin/env bash
+set -e
+
 cmake -B build
 cmake --build build --target benchmark_mem_route
 
 perf_flags="cache-misses,cycles,minor-faults,major-faults"
 executable_route="./build/benchmark_mem_route"
-file_name="benchmark_result.txt"
+file_name1="benchmark_result_perf.txt"
+file_name2="benchmark_result_resources.txt"
 
-touch ./"$file_name"
-perf stat -e "$perf_flags" "$executable_route" "[mult_256][alloc + dealloc]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_256][access]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_8k][alloc + dealloc]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_8k][access]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_128k][alloc + dealloc]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_128k][access]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_8m][alloc + dealloc]" >> "$file_name" 2>&1
-perf stat -e "$perf_flags" "$executable_route" "[mult_8m][access]" >> "$file_name" 2>&1
+run_perf(){
+    local tag="$1"
+
+    perf stat -e "$perf_flags" \
+        "$executable_route" "$tag" >> "$file_name1" 2>&1
+}
+
+run_resources(){
+    local tag="$1"
+
+    /usr/bin/time -v \
+        "$executable_route" "$tag" >> "$file_name2" 2>&1
+}
+
+
+
+tags=(
+    "[mult_256][alloc + dealloc]"
+    "[mult_256][access]"
+    "[mult_8k][alloc + dealloc]"
+    "[mult_8k][access]"
+    "[mult_128k][alloc + dealloc]"
+    "[mult_128k][access]"
+    "[mult_8m][alloc + dealloc]"
+    "[mult_8m][access]"
+)
+
+: > "$file_name1"
+: > "$file_name2"
+
+for tag in "${tags[@]}"; do
+    run_perf "$tag"
+done
+
+
+for tag in "${tags[@]}"; do
+    run_resources "$tag"
+done
+
+
+
+
 
 
 
