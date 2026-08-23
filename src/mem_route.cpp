fcc8df7f7bf5d3898a3846ac5ed88ce004226838
ElectroX79 <jizhuoxian@gmail.com>
2026-08-23 21:43:56 +0200

    refactor: removal of cow philosophy and shared_ptr
    - See docs to understand the reasons for this change.
    - The main reason is that shared_ptr is not a good fit for our use case, and cow philosophy is not needed anymore since we have a better memory management strategy in place.

--- src/mem_route.cpp
+++ src/mem_route.cpp
@@ -1,3 +1,5 @@
+#include "mem/mem_route.hpp"
+
 #include <cstring>
 #include <vector>
 #include <memory>
@@ -11,21 +13,19 @@
 
 namespace atomix::mem::mem_route{
     namespace {
-         [[nodiscard]] std::vector<std::shared_ptr<Buffer>> allocate_chunked(const size_t size, const size_t chunk_size) {
+         [[nodiscard]] std::vector<Buffer> allocate_chunked(const size_t size, const size_t chunk_size, const size_t alignment = 64) {
             assert(size >= chunk_size);
 
-            std::vector<std::shared_ptr<Buffer>> v;
+            std::vector<Buffer> v;
             v.reserve((size / (chunk_size)) + 1);
 
             const size_t remainder = (size % (chunk_size));
 
             for (size_t i = 0; i < size / (chunk_size); ++i) {
-                uint8_t* ptr = aligned_allocator::allocate(chunk_size);
-                v.push_back(std::make_shared<Buffer>(ptr, chunk_size, AllocationType::aligned_alloc));
+                v.push_back(allocate_buffer(chunk_size, AllocationType::aligned_alloc, alignment));
             }
             if (remainder != 0){
-                uint8_t* ptr = aligned_allocator::allocate(remainder);
-                v.push_back(std::make_shared<Buffer>(ptr, remainder, AllocationType::aligned_alloc));
+                v.push_back(allocate_buffer( remainder, AllocationType::aligned_alloc, alignment));
             }
             return v;
         }
@@ -34,17 +34,18 @@ namespace atomix::mem::mem_route{
     constexpr uint64_t paged_min = 64 * kb;
     constexpr size_t one_chunk_size = 64 * kb;
 
-     [[nodiscard]] std::vector<std::shared_ptr<Buffer>> allocate(const size_t size, size_t& chunk_size, const size_t alignment) {
+     [[nodiscard]] std::vector<Buffer> allocate(const size_t size, size_t& chunk_size, const size_t alignment ) {
 
 
         if (size < paged_min) {
-            uint8_t* ptr = aligned_allocator::allocate(size, alignment);
             chunk_size = 0;
-            return std::vector<std::shared_ptr<Buffer>>{std::make_shared<Buffer> (ptr, size, AllocationType::aligned_alloc)};
+            std::vector<Buffer> v;
+            v.push_back(allocate_buffer(size, AllocationType::aligned_alloc, alignment));
+            return v;
         }
 
         chunk_size = one_chunk_size;
-        return allocate_chunked(size, chunk_size);
+        return allocate_chunked(size, chunk_size, alignment);
     }
 
 
@@ -61,6 +62,20 @@ namespace atomix::mem::mem_route{
         }
     }
 
+    [[nodiscard]]Buffer allocate_buffer(const size_t size, const AllocationType alloc_t, const size_t alignment) {
+         switch (alloc_t) {
+
+             case AllocationType::aligned_alloc:
+                 return Buffer(aligned_allocator::allocate(size, alignment), size, alloc_t);
+
+             case AllocationType::none:
+                 std::cerr << "Precondition broken: { alloc_t == AllocationType::none }, on atomix::mem::mem::route::allocate_buffer \n";
+                 std::abort();
+         }
+
+     }
+
+    /*
      [[nodiscard]] std::vector<std::shared_ptr<Buffer>> copy(const std::vector<std::shared_ptr<Buffer>>& ref, const std::vector<CopyType>& copy_types) {
         if (copy_types.size() != ref.size()) {
             std::cerr << "Precondition broken: { copy_types.size() == ref.size() }, on atomix::mem::mem::route::copy_deep \n";
@@ -69,12 +84,12 @@ namespace atomix::mem::mem_route{
         std::vector<std::shared_ptr<Buffer>> aux;
         aux.reserve(ref.size());
 
-        /*
+
         if (ref[0]->get_alloc_t() == AllocationType::none) {
             //needs another strategy, for now that doesn't matter (all uses aligned allocation), but in the future,
             //that could be of some import.
         }
-        */
+
 
 
         for (size_t i = 0; i < copy_types.size(); ++i) {
@@ -98,9 +113,10 @@ namespace atomix::mem::mem_route{
             }
         }
         return aux;
+
     }
 
-
+    */
 
 }
 
