6df9b97d4287cec2bbf8171d02751cbad9c042f5
ElectroX79 <jizhuoxian@gmail.com>
2026-08-25 02:37:58 +0200

    feat: add declaration of atomix::io::parser_csv::parse_header()

--- include/io/parser_csv.hpp
+++ include/io/parser_csv.hpp
@@ -2,8 +2,16 @@
 #define PARSER_CSV_HPP
 
 #include "reader.hpp"
+#include "../data/data_table.hpp"
 
 namespace atomix::io::parser_csv {
+    /** @brief It reads the first row, complete the DataTable with the names,
+     * and return the size of the first row (including line break).
+     *
+     * @param reader A class that contains the ptr, the size of the entire file, and the file descriptor
+     * @param dt The DataTable to introduce info
+     */
+    [[nodiscard]]size_t parse_header(const Reader& reader, DataTable& dt);
 
     
 }
