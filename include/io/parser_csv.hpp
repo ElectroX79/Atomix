a702decfb20d8cbc8bcbf5df257a84220bf137f9
ElectroX79 <jizhuoxian@gmail.com>
2026-08-27 00:44:12 +0200

    feat(ParserCsv): make ParserCsv class friend of DataTable

--- include/io/parser_csv.hpp
+++ include/io/parser_csv.hpp
@@ -4,14 +4,18 @@
 #include "reader.hpp"
 #include "../data/data_table.hpp"
 
-namespace atomix::io::parser_csv {
-    /** @brief It reads the first row, complete the DataTable with the names,
-     * and return the size of the first row (including line break).
-     *
-     * @param reader A class that contains the ptr, the size of the entire file, and the file descriptor
-     * @param dt The DataTable to introduce info
-     */
-    [[nodiscard]]size_t parse_header(const Reader& reader, DataTable& dt);
+namespace atomix::io {
+    class ParserCsv {
+        /** @brief It reads the first row, complete the DataTable with the names,
+         * and return the size of the first row (including line break).
+         *
+         * @param reader A class that contains the ptr, the size of the entire file, and the file descriptor
+         * @param dt The DataTable to introduce info
+         */
+        [[nodiscard]] static size_t parse_header(const Reader& reader, DataTable& dt);
+
+
+    };
 
     
 }
